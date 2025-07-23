396ed10fd1f54217031ebda9995ddd3f93401a79
Nishit079 <pnishit004@gmail.com>
2025-07-23 20:59:09 +0530

    Practical 1

---
+++ Practical 1/1.3 .c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    printf("Hello \nWorld\n");
+    printf("**************\n* Hello World* \n**************\n");
+    printf("\"16/01/2025\"\n");
+    printf("\"Hello World\"\n");
+    printf("16\\\\01\\\\2025\n");
+    printf("\n");
+    printf("Name : Nishit V Patel\n");
+    printf("Phone no : 8200047069\n");
+    printf("Address : Ahmedabad\n");
+    printf("Birth Date : 10/06/2006\n");
+
+    return 0;
+}
